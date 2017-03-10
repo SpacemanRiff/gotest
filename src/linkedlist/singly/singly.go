3d38e53d73d70b33fe7a04307e4121de5fdd16f8
Scott <troutmansm@gmail.com>
2017-03-09 18:59:23 -0600

    AAAAAA

--- src/linkedlist/singly/singly.go
+++ src/linkedlist/singly/singly.go
@@ -25,5 +25,17 @@ func val_at(node Node, des_index int, cur_index int) int {
 }
 
 func Add_Node(node Node, new_val int) Node {
-	
+	last := go_to_last(node)
+	next := Node{val: new_val, next: nil}
+	last.next = &next
+	return node
+}
+
+func go_to_last(node Node) Node {
+	if(node.next != nil){
+		next := node.next
+		return go_to_last(*next)
+	} else {
+		return node
+	}
 }
